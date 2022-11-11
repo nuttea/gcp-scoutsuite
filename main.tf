81dd050a1257fd79b9a3fa56c7629df28b76775f
craythorne <craythorne@google.com>
2022-11-11 07:41:05 +0700

    adjusted SA perms

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "time_sleep" "wait_cloudbuild_sa_iam" {
 
 resource "google_organization_iam_binding" "binding" {
   org_id = data.google_organization.org.org_id
-  role    = "roles/storage.objectAdmin"
+  role    = "roles/storage.objectCreator"
   members = [
     format("serviceAccount:%s", google_service_account.scoutsuite_service_account.email),
   ]
